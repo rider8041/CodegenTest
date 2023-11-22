9e3d01718816d5bfeb6081c611bbb812d402b276
Rider <rider8041@rambler.ru>
2023-11-22 14:28:02 +0300

    addition from file

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.CodeAnalysis.CSharp.Scripting;
 using Microsoft.CodeAnalysis.Scripting;
+using System.Text;
 
 namespace ConsoleApp1
 {
@@ -7,11 +8,42 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Script<object> scripts = CSharpScript.Create("int x = 2 + 3;");
-            scripts.Compile();
-            ScriptState<object> state = scripts.RunAsync().Result;
-            ScriptVariable variable = state.Variables[0];
-            int x = (int)variable.Value;
+            StringBuilder code = new StringBuilder();
+            using (FileStream fileSteream = File.Open("file.txt", FileMode.Open))
+            {
+                using (StreamReader streamReader = new StreamReader(fileSteream))
+                {
+                    string line = "";
+                    do 
+                    {
+                        line = streamReader.ReadLine();
+                    } while (streamReader.ReadLine() != null);
+
+                    string[] fields = line.Split(',');
+
+                    for (int i = 0; i < fields.Length; i++)
+                    {
+                        code.AppendLine($@"int {fields[i]} = {i};");
+                    }
+
+                    code.AppendLine(Environment.NewLine);
+                    var expr = string.Join(" + ", fields);
+                    code.Append($"int result = {expr};");
+                }
+            }
+
+            Script<object> script = CSharpScript.Create(code.ToString());
+            ScriptState<object> state = script.RunAsync().Result;
+            int result = (int)state.Variables.First(x => x.Name == "result").Value;
+
+            Console.ReadKey();
         }
+    }
+
+    public static class Source
+    {
+        public static string Code = "System.Console.WriteLine(\"Hello world\")";
+
+        public static string AnotherCode = $@"";
     }
 }
