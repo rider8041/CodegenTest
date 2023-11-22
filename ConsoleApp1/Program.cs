11d0c25c0f6964192c85cecb75bdeee648692f2c
Rider <rider8041@rambler.ru>
2023-11-22 13:28:06 +0300

    trivial example

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Script<object> scripts = CSharpScript.Create("int x = 2 + 3;");
+            scripts.Compile();
+            ScriptState<object> state = scripts.RunAsync().Result;
+            ScriptVariable variable = state.Variables[0];
+            int x = (int)variable.Value;
+        }
+    }
+}
